77d12db07479046cbea01fd3624cfb3c86f2add6
Josh Wells <wellsy9722@gmail.com>
2024-11-12 23:51:51 +1100

    Added report comments and merged heading.py

--- heading.py
+++ heading.py
@@ -13,131 +13,6 @@ import matplotlib.pyplot as plt
 from torch.utils import data
 from PIL import Image
 
-
-#___________________________________________
-#ResNet
-
-#For ResNet 18, 34, use two 3 x 3 convolution
-class BasicBlock(torch.nn.Module):
-    expansion = 1
-
-    def __init__(self, in_planes, planes, stride = 1):
-        super().__init__()
-        self.conv1 = torch.nn.Conv2d(in_planes, planes, kernel_size = 3, 
-            stride = stride, padding = 1, bias = False)
-        self.bn1 = torch.nn.BatchNorm2d(planes)
-        self.conv2 = torch.nn.Conv2d(planes, planes, kernel_size =3,
-            stride = 1, padding = 1, bias = False)
-        self.bn2 = torch.nn.BatchNorm2d(planes)
-        self.shortcut = torch.nn.Sequential()
-
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = torch.nn.Sequential(
-                torch.nn.Conv2d(in_planes, self.expansion * planes,
-                    kernel_size = 1, stride = stride, bias = False),
-                torch.nn.BatchNorm2d(self.expansion*planes)
-                )
-
-    def forward(self, x):
-        out = torch.nn.functional.relu(self.bn1(self.conv1(x)))
-        out = torch.nn.functional.relu(self.bn2(self.conv2(out)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = torch.nn.functional.relu(out)
-        return out
-
-class ResNet(torch.nn.Module):
-    def __init__(self, block, num_blocks, input_channels = 1, num_classes = 10):
-        super().__init__()
-        self.in_planes = 64
-
-        self.conv1 = torch.nn.Conv2d(input_channels, 64, kernel_size = 3,
-            stride = 1, padding = 1, bias = False)
-        self.bn1 = torch.nn.BatchNorm2d(64)
-
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride = 1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride = 2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride = 2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride = 2)
-        self.linear = torch.nn.Linear(512 * block.expansion, num_classes)
-
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
-        return torch.nn.Sequential(*layers)
-
-    def forward(self, x):
-        out = torch.nn.functional.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = torch.nn.functional.avg_pool2d(out, 4)
-
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
-
-#___________________________________________
-#VGG 11
-
-class VGG(nn.Module):
-
-    def __init__(self, features, num_classes=10, init_weights=True):
-        super(VGG, self).__init__()
-        self.features = features
-        self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(4096, num_classes),
-        )
-        if init_weights:
-            self._initialize_weights()
-
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
-
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-                if m.bias is not None:
-                    m.bias.data.zero_()
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
-
-
-def make_layers(cfg, input_channels = 1, batch_norm=False):
-    layers = []
-    in_channels = input_channels
-    for v in cfg:
-        if v == 'M':
-            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-        else:
-            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
-            if batch_norm:
-                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
-            else:
-                layers += [conv2d, nn.ReLU(inplace=True)]
-            in_channels = v
-    return nn.Sequential(*layers)
-
-
 class AlexNet(torch.nn.Module):   
     def  __init__(self, input_channels = 1, output_size = 10):
         super().__init__()
@@ -184,36 +59,6 @@ class AlexNet(torch.nn.Module):
         x = conv5_out.view(conv5_out.size(0), -1)
         out = self.dense(x)
         return out
-
-class FlameSet(data.Dataset):
-    def __init__(self,root, image_size = 28):
-        self.imgs_list = os.listdir(root)
-        self.imgs = [os.path.join(root,k) for k in self.imgs_list]
-        self.labels = [k.split('-')[0] for k in self.imgs_list]
-        self.transforms = transforms.Compose([
-            transforms.Resize(image_size),
-            transforms.ToTensor()
-            ])
-
-    def __getitem__(self, index):
-        img_path = self.imgs[index]
-        pil_img = Image.open(img_path)
-        pil_img = pil_img.convert('L')
-        if self.transforms:
-            data = self.transforms(pil_img)
-        else:
-            pil_img = np.asarray(pil_img)
-            data = torch.from_numpy(pil_img)
-        return data
-
-    def __len__(self):
-        return len(self.imgs)
-
-    def get_image_list(self):
-        return self.imgs_list
-
-    def get_image_label(self):
-        return self.labels
 
 def resize_image(input_size = 28):
     transform = transforms.Compose([
@@ -284,7 +129,6 @@ def train_model(net, train_loader, LR, epochs=1, number_of_images=None):
 
 
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
-
 def test_model(net, test_loader, number_of_images=None, class_label=None):
     net.eval()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -408,7 +252,3 @@ def predict_image(net, input_image, objective_list, num_of_prediction=1, true_la
         "Confidence": confidence_scores,
         "True Label": true_label
     }
-
-
-
-
